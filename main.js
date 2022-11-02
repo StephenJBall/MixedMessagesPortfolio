581e6b7c35e8081abc370991eb2751aae5fcc988
StephenJBall <stephenjb94@gmail.com>
2022-11-02 16:27:01 +0000

    Added second and third functions

--- main.js
+++ main.js
@@ -7,3 +7,21 @@ const chooseMeat = arr => {
     let i = Math.floor(Math.random() * arr.length)
     console.log(`The protein for this meal will be ${arr[i]}!`)
 };
+
+const chooseSides = arr => {
+    let i = Math.floor(Math.random() * arr.length)
+    console.log(`This should be accompanied by ${arr[i]}!`)
+};
+
+const chooseCuisine = arr => {
+    let i = Math.floor(Math.random() * arr.length)
+    console.log(`Your meal should be inspired by ${arr[i]} cuisine!`)
+};
+
+generateNewMeal = () => {
+    chooseMeat(mealProtein);
+    chooseSides(mealSides);
+    chooseCuisine(mealCuisine);
+}
+
+generateNewMeal();
